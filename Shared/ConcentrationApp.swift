30f4fe3d76c803145b2d30ce162acd296c17dff2
Katrin Mailyan <katin.mailyan31@gmail.com>
2020-12-08 15:17:55 +0300

    Initial Commit

---
+++ Shared/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Shared
+//
+//  Created by Катрин Маилян on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
